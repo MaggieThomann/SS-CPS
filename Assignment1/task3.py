ebdb4d464db3e90b33c3e1ec2f5f014ec7499f2c
MaggieThomann <mmthomann@gmail.com>
2018-02-04 19:23:27 -0500

    ✅ Task 3 Complete:  Retrieved tweets with keywords and geolocation and generated files task3output-geofile.txt and task3output-keywordtweets.txt

--- Assignment1/task3.py
+++ Assignment1/task3.py
@@ -7,6 +7,37 @@ Description:	Collecting tweets in real-time filtered by specified keywords and g
 '''
 # Libary Imports
 import tweepy
+import json
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+
+# Class for listener
+class Listener(StreamListener):
+	def __init__(self, type):
+		self.keywordtweets = []
+		self.geolocationtweets = []
+		self.type = type
+
+	def on_data(self, data):
+		
+		all_data = json.loads(data)
+		tweet = all_data["text"]
+		if self.type == "keywords":
+		 	self.keywordtweets.append(tweet)
+			if len(self.keywordtweets) == 50:
+				return False
+			else:
+				return True 
+		elif self.type == "geolocation":
+		 	self.geolocationtweets.append(tweet)
+			if len(self.geolocationtweets) == 50:
+				return False
+			else:
+				return True 
+
+
+	def on_error(self, status):
+		 print status
 
 # Keys for API Access
 consumer_key = "cnHTXNSLFTkFlCK8yXKpfA4hP"
@@ -26,12 +57,38 @@ keywords = ["Indiana", "weather"]
 geolocation = [-86.33,41.63,-86.20,41.74]
 
 # Create output file
-keywordfile = open('keywordtweets.txt','w')
-geofile = open('geofile.txt','w')
+keywordfile = open('task3output-keywordtweets.txt','w')
+geofile = open('task3output-geofile.txt','w')
 
-# Create the stream
-listener = StdOutListener()
+# Create the listener
+listener = Listener("keywords")
+geolistener = Listener("geolocation")
+
+# Create the keyword stream
 stream = Stream(auth, listener)
 
+# Create the geo stream
+geostream = Stream(auth, geolistener)
+
 # Filter the stream
-stream.filter(track=keywords)
+stream.filter(track=keywords, languages=["en"])
+geostream.filter(locations=geolocation, languages=["en"])
+
+# Add it to the file
+count = 1
+for tweet in listener.keywordtweets:
+	keywordfile.write(str(count) + ":  "+tweet.encode('utf-8')+"\n"+"\n")
+	count += 1
+
+count = 1
+for tweet in geolistener.geolocationtweets:
+	geofile.write(str(count) + ":  "+tweet.encode('utf-8')+"\n"+"\n")
+	count += 1
+
+# Close the file
+keywordfile.close()
+geofile.close()
+
+
+
+
