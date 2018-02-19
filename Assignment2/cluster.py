f720d789b60990013a7ec73a0e31cd80a3ae673a
MaggieThomann <mmthomann@gmail.com>
2018-02-18 21:11:51 -0500

    Reassigning the centroids now

--- Assignment2/cluster.py
+++ Assignment2/cluster.py
@@ -6,30 +6,106 @@ Date:		Monday Feb 19, 2018
 Description:	Implements k-means clustering using the Jaccard distance 
 		for a set of tweets
 '''
+import json
 
-# distance:	Computes the Jaccard distance between a pair of tweets
+# distance:			Computes the Jaccard distance between a pair of tweets
+def distance(a, b):
+	intersection = list(set(a) & set(b))
+	intersection_length =len(intersection)
+	union = list(set(a) | set(b))
+	union_length = len(union)
+	distance = round(1 - (float(intersection_length)/union_length), 4)
+	return distance
 
-# cluster:	Clusters the tweets into groups according to their jaccard distance
-#		and centroids
+# compute_new_centroids:	Computes new centroids based on how the data points
+#				were assigned to the clusters.
+def compute_new_centroids(tweets, mean_indices):
+	if len(mean_indices) > 0:
+		# Construct the text from the indices
+		mean_tweets = []
+		for index in mean_indices:
+			mean_tweets.append(tweets[index]['text'].lower().split())
+
+		# Get the distances
+		distances = []
+		for i in range(len(mean_indices)):
+			inner_distances = []
+			for j in range(len(mean_indices)):
+				dist = distance(mean_tweets[i], mean_tweets[j])
+				inner_distances.append(dist)
+			distances.append(inner_distances)
+		# Sum all the distances
+		print distances
+		sums = []
+		for inner_distance in distances:
+			sum_dist = sum(inner_distance)
+			sums.append(sum_dist)
+		#print "sums"
+		#print sums
+	return sums
+
+
+# cluster:			Clusters the tweets into groups according to their jaccard distance
+#				and centroids
 def cluster(k, tweets, seeds):
-	print "k is "+str(k)
+
+	# Perform the clustering
+	for i in range(len(seeds)):
+
+		# Get the tweets for each centroid
+		centroid_tweets = []
+		for tweet in tweets:
+			id = tweet['id']
+			for seed in seeds:
+				if str(id) == str(seed):
+					# Make the tweet a list of words
+					list_tweet = tweet['text'].lower().split()
+					centroid_tweets.append(list_tweet)
+
+		# Create each cluster
+		cluster = []
+		for tweet in tweets:
+			distances = []
+			for j in range(k):
+				dist = distance(tweet['text'].lower().split(), centroid_tweets[j])
+				distances.append(dist)
+			minimum_distance  = min(distances)
+			index_of_minimum_distance = distances.index(minimum_distance)
+ 			cluster.append(index_of_minimum_distance)
+
+ 		indices = []
+ 		for kmean in range(k):
+ 			indices_list = []
+			for tweet_id, cluster_id in enumerate(cluster):
+				if cluster_id == kmean:
+					indices_list.append(tweet_id)
+			indices.append(indices_list)
+			mean_indices=indices[kmean]
+
+			print "MEAN INDICES:"
+			print mean_indices
+		
+			# Compute the new centroids based on the updated assignment of data points to clusters.
+ 			new_centroid_sums = compute_new_centroids(tweets, mean_indices)
 
 
 
-# read_file:	Reads in the file of tweets and returns it as a list
+# read_file:			Reads in the file of tweets and returns it as a list of dicts
 def read_file(file):
 	f = open(file)
 	tweets = []
 	for line in f:
-		tweets.append(line)
+		tweet = json.loads(line)
+		tweets.append(tweet)
 	return tweets
 
-# read_seeds:	Reads in the initial seeds from the file and returns it as a list
+# read_seeds:			Reads in the initial seeds from the file and returns it as a list
 def read_seeds(file):
 	f = open(file)
 	seeds = []
 	for line in f:
-		seed = line[:-1]
+		seed = line.replace(',', '')
+		seed = seed.replace('\n', '')
 		seeds.append(seed)
 	return seeds
 
